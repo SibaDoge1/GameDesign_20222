038e4ef34ce75c246a453409b4c37bc91b08a0b5
taesik <jts1535@naver.com>
2022-12-11 16:45:50 +0900

    hpbar

---
+++ Assets/Health.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class Health : MonoBehaviour
+{
+    public int startingHealth = 100;
+    public int currentHealth;
+    public Slider healthSlider;
+    public Image damageImage;
+    public AudioClip deathClip;
+    public float flashSpeed = 5f;
+    public Color flashColour = new Color(1f, 0f, 0f, 0.1f);
+
+    private AudioSource playerAudio;
+
+    private bool isDead;
+    private bool damaged;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerAudio = GetComponent<AudioSource>();
+
+        currentHealth = startingHealth;
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (damaged)
+        {
+            damageImage.color = flashColour;
+        }
+        else
+        {
+            damageImage.color = Color.Lerp(damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
+        }
+        damaged = false;
+    }
+    private void OnCollisionStay(Collision other)
+    {
+        if (other.transform.CompareTag("Ground")&&!isDead)
+        {
+            TakeDamage(1);
+        }
+    }
+
+    public void TakeDamage(int amount)
+    {
+        damaged = true;
+
+        currentHealth -= amount;
+        healthSlider.value = currentHealth;
+
+        playerAudio.Play();
+
+        if(currentHealth <=0 && !isDead)
+        {
+            Death();
+        }
+    }
+    void Death()
+    {
+        isDead = true;
+
+        playerAudio.clip = deathClip;
+        playerAudio.Play();
+    }
+
+    public void RestartLevel()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+}
